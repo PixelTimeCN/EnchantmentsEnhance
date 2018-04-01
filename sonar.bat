8446a4558ceecf0f038b0727064939e74e144b55
HealPot <31719556+UnChartedDev@users.noreply.github.com>
2018-04-01 17:37:23 -0400

    [UPDATE] Recode in Kotlin!

--- sonar.bat
+++
@@ -1,3 +0,0 @@
-mvn sonar:sonar 
-  -Dsonar.organization=healpot-github 
-  -Dsonar.host.url=https://sonarcloud.io
---
+++ src/main/kotlin/org/pixeltime/enchantmentsenhance/main/Main.java
@@ -0,0 +1,4 @@
+package org.pixeltime.enchantmentsenhance.main;
+
+public class Main {
+}
